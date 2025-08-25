e70846687fa48b3858c38b94f99a55f312d7c6b9
Anisa Nurjanah <anisanurjanah2705@gmail.com>
2025-08-25 14:58:48 +0700

    header and hero layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import { useState } from 'react'
+import Header from './components/Header'
+import Hero from './components/Hero'
 
 function App() {
   return (
     <>
-      
+    <div class="container max-w-screen-xl mx-auto p-4">  
+      <Header />
+      <Hero />
+    </div>
     </>
   )
 }
