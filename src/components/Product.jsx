74de6e730c07aa6a4265321348842b7126e130fb
Anisa Nurjanah <anisanurjanah2705@gmail.com>
2025-08-26 16:22:32 +0700

    products json

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,4 +1,10 @@
+import product from '@/data/products.json'
+
 export default function Product() {
+    const products = product.data;
+    const productHighlight = products[0];
+    const productItem = products.slice(1);
+
     return (
         <>
             <section id="product">
@@ -14,60 +20,64 @@ export default function Product() {
                         </p>
                     </header>
 
-                    <ul className="product-hightlight-grid">
+                    <ul className="product-highlight-grid">
                         <li className="product-highlight">
-                            <a href="#" className="product-highlight-link">
-                                <div className="product-highlight-image-wrapper">
-                                    <img
-                                        src="https://images.unsplash.com/photo-1593795899768-947c4929449d?auto=format&fit=crop&w=2672&q=80"
-                                        alt=""
-                                        className="product-highlight-img-primary"
-                                    />
-                                    <img
-                                        src="https://images.unsplash.com/photo-1593795899630-b6033c0fa58d?auto=format&fit=crop&w=774&q=80"
-                                        alt=""
-                                        className="product-highlight-img-hover"
-                                    />
-                                </div>
+                            {
+                                productHighlight && (
+                                    <a href={productHighlight.link} className="product-highlight-link">
+                                        <div className="product-highlight-image-wrapper">
+                                            <img
+                                                src={productHighlight.image}
+                                                alt=""
+                                                className="product-highlight-img-primary"
+                                            />
+                                            <img
+                                                src="https://images.unsplash.com/photo-1593795899630-b6033c0fa58d?auto=format&fit=crop&w=774&q=80"
+                                                alt=""
+                                                className="product-highlight-img-hover"
+                                            />
+                                        </div>
 
-                                <div className="product-highlight-content">
-                                    <h3 className="product-highlight-title">Paket Premium Dimsum</h3>
-
-                                    <p className="product-highlight-description">
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sequi dicta impedit
-                                        aperiam ipsum!
-                                    </p>
-
-                                    <span
-                                        className="product-highlight-btn"
-                                    >
-                                        Shop Now
-                                    </span>
-                                </div>
-                            </a>
+                                        <div className="product-highlight-content">
+                                            <h3 className="product-highlight-title">{productHighlight.title}</h3>
+                                            <p className="product-highlight-description">{productHighlight.description}</p>
+                                            <button
+                                                onClick={() => productHighlight.link}
+                                                className="product-highlight-btn"
+                                            >
+                                                Learn More
+                                            </button>
+                                        </div>
+                                    </a>
+                                )
+                            }
                         </li>
                     </ul>
 
                     <ul className="product-card-grid">
-                        {[1, 2, 3].map((i) => (
-                            <li key={i}>
-                            <a href="#" className="product-card">
-                                <img
-                                    src="https://images.unsplash.com/photo-1485955900006-10f4d324d411?q=80&w=2672&auto=format&fit=crop"
-                                    alt=""
-                                    className="product-card-img"
-                                />
-                                <div className="product-card-body">
-                                    <strong className="product-card-title"> Aloe Vera </strong>
-                                    <p className="product-card-desc">
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet officia rem vel voluptatum in
-                                        eum vitae aliquid at sed dignissimos.
-                                    </p>
-                                    <span className="product-card-btn">Learn More</span>
-                                </div>
-                            </a>
-                            </li>
-                        ))}
+                        {
+                            productItem.map((product) => (
+                                <li key={product.id}>
+                                    <a href={product.link} className="product-card">
+                                        <img
+                                            src={product.image}
+                                            alt=""
+                                            className="product-card-img"
+                                        />
+                                        <div className="product-card-body">
+                                            <strong className="product-card-title">{product.title}</strong>
+                                            <p className="product-card-description">{product.description}</p>
+                                            <button
+                                                onClick={() => product.link}
+                                                className="product-card-btn"
+                                            >
+                                                Learn More
+                                            </button>
+                                        </div>
+                                    </a>
+                                </li>
+                            ))
+                        }
                     </ul>
                 </div>
             </section>
